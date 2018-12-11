60bc100428a30a7f5f7d2e6a73f07e2b60af1ace
Argenis Gómez <argejgomez@gmail.com>
2018-12-11 16:01:51 -0430

    edit-profile

---
+++ src/pages/edit-profile/edit-profile.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+/**
+ * Generated class for the EditProfilePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-edit-profile',
+  templateUrl: 'edit-profile.html',
+})
+export class EditProfilePage {
+	myForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  	public formBuilder: FormBuilder) {
+  	this.myForm = this.createMyForm();
+  }
+
+  saveData(){
+    console.log(this.myForm.value);
+  }
+  
+  private createMyForm(){
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      dateBirth: ['', Validators.required],
+      passwordRetry: this.formBuilder.group({
+        password: ['', Validators.required],
+        passwordConfirmation: ['', Validators.required]
+      }),
+      gender: ['', Validators.required],
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditProfilePage');
+  }
+
+}
